be83ae7cc69921e7cdd274e80d8aad6a6f57642a
nyctef <nyctef@nyctef.com>
2020-12-04 16:18:40 +0000

    add a crosshair and debug gun

---
+++ faim/Assets/Gun.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (!Physics.Raycast(transform.position, transform.forward, out hit, 100)) {
+            return;
+        }
+
+        Debug.Log(hit.transform.name);
+    }
+}
